bb5a4398580615a3ce4ae723a0304539f1940951
Katsumi Sunahara <s23c1078uq@s.chibakoudai.jp>
2024-11-27 09:55:19 +0900

    keisann

---
+++ kaijou.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def factorial(n):
+    if n < 0:
+        return "負の数には階乗の定義がありません。"
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        result = 1
+        for i in range(2, n + 1):
+            result *= i
+        return result
+
+# 自分の手で入力する
+try:
+    number = int(input("階乗を計算する数を入れてください:"))
+    print(f"{number} の階乗は{factorial(number)}")
+except ValueError:
+    print("整数を入力してください。")
